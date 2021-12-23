ec2ca1001a3ea3800fa7cf07f8ae3e4d51ca3e2f
Nene Matsuki <nenematsuki@gmail.com>
2021-12-23 21:36:20 +0000

    adjusted prediction time to json config

--- get_prediction_time.py
+++ get_prediction_time.py
@@ -3,8 +3,6 @@ from deep_emotion_recognition import DeepEmotionRecognizer
 import librosa
 import os
 import glob
-from sys import byteorder
-import sys
 from array import array
 from struct import pack
 from sklearn.ensemble import GradientBoostingClassifier, BaggingClassifier, RandomForestClassifier, AdaBoostClassifier
@@ -18,6 +16,7 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import time
 import statistics
+import json
 
 from utils import get_best_estimators
 
@@ -29,62 +28,39 @@ if __name__ == "__main__":
     estimators = get_best_estimators(True)
     estimators_str, estimator_dict = get_estimators_name(estimators)
     print(estimators_str)
-    import argparse
-    parser = argparse.ArgumentParser(description="""
-                                    Testing emotion recognition system using your voice, 
-                                    please consider changing the model and/or parameters as you wish.
-                                    """)
-    parser.add_argument("-e", "--emotions", help=
-                                            """Emotions to recognize separated by a comma ',', available emotions are
-                                            "neutral", "calm", "happy" "sad", "angry", "fear", "disgust", "ps" (pleasant surprise)
-                                            and "boredom", default is "sad,neutral,happy"
-                                            """, default=sys.argv[1] )
-    parser.add_argument("-m", "--model", help=
-                                        """
-                                        The model to use, 8 models available are: "SVC","AdaBo
-                                        ostClassifier","RandomForestClassifier","GradientBoost
-                                        ingClassifier","DecisionTreeClassifier","KNeighborsCla
-                                        ssifier","MLPClassifier","BaggingClassifier", default
-                                        is "BaggingClassifier"
-                                        """.format(estimators_str), default=sys.argv[2])
 
-    parser.add_argument("--tess_ravdess", default = True)       # use tess/ravdess dataset
-    parser.add_argument("--classification", default = True)     # use classification
-    parser.add_argument("--custom_db", default = True)          # use custom dataset
-    parser.add_argument("--emodb", default = True)              # use emodb
-    parser.add_argument('--model_name', default = os.path.join(sys.argv[4],sys.argv[2]))
-
+    with open('predict_single.json') as config_file:
+        data = json.load(config_file)
+        model =     data["model"].format(estimators_str)
+        emotions =  data['emotions'].split(",")
+        frequency = data["frequency"]
+        features =  data["features"].split(",")
+        model_ver = data["model_ver"]
     
+    model_name = os.path.join(model_ver,model)
 
-    # Parse the arguments passed
-    args, unknown = parser.parse_known_args()
-
-    features = ["mfcc", "chroma", "mel", "contrast", "tonnetz"]
-
-
+    # initialise array to start time talen and 
     time_taken = []
     duration = []
 
-      # Random Forest, Adaboost  Classifier not working
 
-       # if classifier is SVC need to parse probability as true to display probability
-    if(sys.argv[2] == "SVC"):
-        detector = EmotionRecognizer(model = SVC(probability = True) , emotions=args.emotions.split(","), model_name = args.model_name,  features=features , verbose=0)
+    # if classifier is SVC need to parse probability as true to display probability
+    if(model == "SVC"):
+        detector = EmotionRecognizer(model = SVC(probability = True) , emotions=emotions, model_name = model_name,  features=features , verbose=0)
 
     # similar for decision tree classifier 
-    elif(sys.argv[2] == "DecisionTreeClassifier"):
-        detector = EmotionRecognizer(model = DecisionTreeClassifier() , emotions=args.emotions.split(","), model_name = args.model_name, features=features, verbose=0)
+    elif(model == "DecisionTreeClassifier"):
+        detector = EmotionRecognizer(model = DecisionTreeClassifier() , emotions=emotions, model_name = model_name, features=features, verbose=0)
     
-    elif(sys.argv[2] == "RNN"):
-        detector = DeepEmotionRecognizer(emotions=(sys.argv[1]).split(","), emodb = True, customdb = True, n_rnn_layers=2, n_dense_layers=2, rnn_units=128, dense_units=128)
+    elif(model == "RNN"):
+        detector = DeepEmotionRecognizer(emotions=emotions, emodb = True, customdb = True, n_rnn_layers=2, n_dense_layers=2, rnn_units=128, dense_units=128)
 
     else:
-        detector = EmotionRecognizer(estimator_dict[args.model] , emotions=args.emotions.split(","), model_name = args.model_name, features=features, verbose=0)
+        detector = EmotionRecognizer(estimator_dict[model] , emotions=emotions, model_name = model_name, features=features, verbose=0)
 
 
     # for the filepath containing that emotion
     emotions = ["neutral","calm","happy","sad","angry","fear",'disgust','ps','boredom']
-    #predictions = detector.predict_proba('01_01_01_02_dogs-sitting_happy.wav')
 
     for emotion in emotions:
         for filepath in glob.iglob(os.path.join("predict_from_audio", f"emotion testing audio 16k", f"{emotion}/*")):
@@ -92,15 +68,14 @@ if __name__ == "__main__":
             # record emotion to be predicted and if the prediction was correct
             duration.append(librosa.get_duration(filename = filepath))
 
-            # record prediction probability
+            # record prediction probability and time
             start_predict = time.perf_counter()
             predictions = detector.predict_proba(filepath)
             end_predict = time.perf_counter() 
 
             time_taken.append(end_predict - start_predict)
 
-    
-  
+    # record medfians
     median_time = statistics.median(time_taken)
     median_length = statistics.median(duration)
 
@@ -111,7 +86,7 @@ if __name__ == "__main__":
 
     fig = plt.figure()
     
-
+    # plot for time taken to predict 
     ax1 = fig.add_subplot(gs[0, 0])
     y, x, _ = ax1.hist(x = time_taken, bins = bars)
     ax1.plot([median_time, median_time],[0, y.max()], label = f"median: {round(median_time,2)} seconds")
@@ -119,6 +94,7 @@ if __name__ == "__main__":
     ax1.set_ylabel("frequency")
     ax1.legend(loc="upper right")
 
+    # plot for duration of audio
     ax2 = fig.add_subplot(gs[0, 1])
     y,x,_ = ax2.hist(x = duration, bins = bars)
     ax2.plot([median_length,median_length],[0, y.max()], label = f"median: {round(median_length,2)} seconds")
@@ -126,6 +102,7 @@ if __name__ == "__main__":
     ax2.set_ylabel("frequency")
     ax2.legend(loc="upper right")
 
+    # plot for each audio and the length it tool to predict 
     ax3 = fig.add_subplot(gs[1, :])
     ax3.plot(samples, time_taken, label = "Time taken to predict audio")
     ax3.plot(samples, duration, label = "length of audio")
@@ -133,7 +110,7 @@ if __name__ == "__main__":
     ax3.set_ylabel("time (seconds)")
     ax3.legend(loc="upper right")
 
-    fig.suptitle(f"Time taken to predict audio at {sys.argv[4]} using {sys.argv[2]}")
+    fig.suptitle(f"Time taken to predict audio at {frequency} using {model}")
     
     plt.tight_layout()
     plt.show()
