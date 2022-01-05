cd2704ea13b8057a1c7958d2a9b0407e62b88f26
Nene Matsuki <nenematsuki@gmail.com>
2022-01-05 18:13:37 +1300

    edited visualisation

--- get_prediction_time.py
+++ get_prediction_time.py
@@ -3,7 +3,7 @@ from deep_emotion_recognition import DeepEmotionRecognizer
 import librosa
 import os
 import glob
-from array import array
+import numpy as np
 from struct import pack
 from sklearn.ensemble import GradientBoostingClassifier, BaggingClassifier, RandomForestClassifier, AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -17,6 +17,9 @@ import matplotlib.gridspec as gridspec
 import time
 import statistics
 import json
+import pandas as pd
+from scipy.stats import gaussian_kde
+import seaborn as sns
 
 from utils import get_best_estimators
 
@@ -77,40 +80,77 @@ if __name__ == "__main__":
 
     # record medfians
     median_time = statistics.median(time_taken)
-    median_length = statistics.median(duration)
+    mean_time = statistics.mean(time_taken)
 
-    gs = gridspec.GridSpec(2, 2)
+    median_length = statistics.median(duration)
+    mean_length = statistics.mean(duration)
+
+    index = np.argsort(duration)
+    sorted_duration = np.sort(duration)
+    sorted_time_taken = np.zeros(len(index))
+    for n in index:
+        sorted_time_taken[n] = time_taken[n]
+
+
+    #gs = gridspec.GridSpec(3, 1)
+    gs = dict(width_ratios=[1, 1], height_ratios=[1, 1, 2])
+    fig, axd = plt.subplot_mosaic([['upper left', 'right'],
+                               ['mid left', 'right'],
+                               ['lower','lower']],
+                              gridspec_kw=gs,
+                              constrained_layout=True)
+    
 
     bars = 30
     samples = list(range(1,42))
 
-    fig = plt.figure()
+    #fig = plt.figure()
     
-    # plot for time taken to predict 
-    ax1 = fig.add_subplot(gs[0, 0])
-    y, x, _ = ax1.hist(x = time_taken, bins = bars)
-    ax1.plot([median_time, median_time],[0, y.max()], label = f"median: {round(median_time,2)} seconds")
+
+    ax1  = axd['upper left']
+    #y, x, _ = ax1.hist(x = time_taken, density = True, bins = bars)
+    sns.kdeplot(x = time_taken, ax = ax1)
+    sns.histplot(x = time_taken, stat = 'density', bins = 15, ax = ax1)
+
+    ax1.axvline(x = median_time, color = 'y' , label = f"median: {round(median_time,2)} seconds")
+    ax1.axvline(x = mean_time, color = 'm', label = f"mean: {round(mean_time,2)} seconds")
     ax1.set_xlabel("Time taken to predict (s)")
-    ax1.set_ylabel("frequency")
+    ax1.set_ylabel("Probability density")
     ax1.legend(loc="upper right")
 
     # plot for duration of audio
-    ax2 = fig.add_subplot(gs[0, 1])
-    y,x,_ = ax2.hist(x = duration, bins = bars)
-    ax2.plot([median_length,median_length],[0, y.max()], label = f"median: {round(median_length,2)} seconds")
+    #ax2 = fig.add_subplot(gs[1, 0])
+    ax2 = axd['mid left']
+    sns.kdeplot(x = duration, ax = ax2)
+    sns.histplot(x = duration, bins = 10, ax = ax2)
+
+    ax2.axvline(x = median_length, color = 'y', label = f"median: {round(median_length,2)} seconds")
+    ax2.axvline(x = mean_length, color = 'm', label = f"mean: {round(mean_length,2)} seconds")
     ax2.set_xlabel("Length of audio (s)")
-    ax2.set_ylabel("frequency")
+    ax2.set_ylabel("Probability Density")
     ax2.legend(loc="upper right")
 
     # plot for each audio and the length it tool to predict 
-    ax3 = fig.add_subplot(gs[1, :])
-    ax3.plot(samples, time_taken, label = "Time taken to predict audio")
-    ax3.plot(samples, duration, label = "length of audio")
-    ax3.set_xlabel("Audio samples")
-    ax3.set_ylabel("time (seconds)")
-    ax3.legend(loc="upper right")
+
+    #ax3 = fig.add_subplot(gs[2, :])
+    ax3 = axd['lower']
+    ax4 = ax3.twinx()
+    lns1 = ax3.plot(samples, time_taken, label = "Time taken to predict audio")
+    lns2 = ax4.plot(samples, sorted_duration, '-r', label = "length of audio")
+    ax4.set_ylabel("length of audio (seconds)")
+    ax3.set_xlabel("Audio in ascending order by length")
+    ax3.set_ylabel("time taken to predicr(seconds)")
+    lns = lns1 + lns2
+    labs = [l.get_label() for l in lns]
+    ax3.legend(lns, labs, loc="upper left")
 
     fig.suptitle(f"Time taken to predict audio at {frequency} using {model}")
+
+
+    ax5 = axd['right']
+    ax5.plot(time_taken, duration, 'r.')
+    ax5.set_xlabel('time taken to predict audio (s)')
+    ax5.set_ylabel('length of audio (s)')
     
     plt.tight_layout()
     plt.show()
