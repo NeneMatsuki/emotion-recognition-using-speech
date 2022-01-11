9bfd55ab9aed3f6f9f3ff3fa950d476d58a6c3e8
Nene Matsuki <nenematsuki@gmail.com>
2022-01-12 12:14:30 +1300

    Combined predicting multiple and renamed files

--- use_audio_to_predict.py
+++ use_audio_to_predict.py
@@ -2,6 +2,7 @@ from emotion_recognition import EmotionRecognizer
 from deep_emotion_recognition import DeepEmotionRecognizer
 import json
 import os
+import glob
 from sys import byteorder
 import sys
 from array import array
@@ -13,6 +14,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 import time
 import librosa
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
 
 from utils import get_best_estimators
 
@@ -48,19 +51,20 @@ if __name__ == "__main__":
             test_settings = data["Testing settings"][0]
             test_mode =     test_settings["Test mode"].lower()
 
+            # create detector instance
+            if(model == "DecisionTreeClassifier"):
+                detector = EmotionRecognizer(model = DecisionTreeClassifier() , emotions=emotions, model_name = model_name, features=features, verbose=0)
+            else:
+                detector = EmotionRecognizer(estimator_dict[model] , emotions=emotions, model_name = model_name, features=features, verbose=0)
+
             # if predicting a single audio
             if(test_mode == 'single'):
 
                 # load settings and record length odf audio
                 single_settings = test_settings["Testing single"][0]
                 audio = single_settings["Audio directory"]
-                print(f'\nLength of audio recorded: {librosa.get_duration(filename = audio)} seconds')
-
-                # create detector instance
-                if(model == "SVC"):
-                    detector = EmotionRecognizer(model = estimator_dict[model] , emotions=emotions, model_name = model_name,  features=features , verbose=0)
-                else:
-                    detector = EmotionRecognizer(estimator_dict[model] , emotions=emotions, model_name = model_name, features=features, verbose=0)
+                print(f'\nChosen to test a single audio using {model} trained on {model_ver}')
+                print(f'Length of audio: {librosa.get_duration(filename = audio)} seconds')
 
                 #predict from filename passed in args
                 start_predict = time.perf_counter()
@@ -72,17 +76,142 @@ if __name__ == "__main__":
                 maximum = max(result, key=result.get)
                 max_value = result[maximum]
                 del result[maximum]
-
                 second = max(result, key=result.get)
                 second_value = result[second]
 
                 print(f"\nfirst prediction  : {maximum} \nsecond prediction : {second} \ndifference is {(max_value - second_value)*100} %")
-                print(f"\nTime it took to predict: {end_predict - start_predict} s")
+                print(f"\nTime it took to predict: {(end_predict - start_predict)*1000} ms")
         
+            # if predicting multiple audio
             else:
                 multiple_settings = test_settings["Testing multiple"][0]
-                output = multiple_settings["output"][0]
+                output = multiple_settings["output"]
+
+                if(output == "excel"):
+                    # initialise workbook 
+                    wb = load_workbook('predict_from_audio/prediction.xlsx')
+                    wb.remove(wb['predictions'])
+                    wb.create_sheet('predictions')
+                    sheet = wb['predictions']
+                    sheet["A1"] = "emotion to predict"
+                    sheet["B1"] = "result"
+
+                    rows = 2
+                    cols = 1
+
+                    sheet[get_column_letter(cols) + str(rows)] = "sm audio"
+                    rows += 1
+
+                    # Predict SM audio 
+                    for i in range(len(emotions)):
+
+                        # for the filepath containing that emotion
+                        for filepath in glob.iglob("predict_from_audio" + os.sep + f"emotion testing audio {model_ver[:3]}" + os.sep + emotions[i] + os.sep + "/*"):
+                            # record the emotion in the excel sheet
+                            sheet[get_column_letter(i + 3) + "1"] =  emotions[i]
+
+                            # calculate probability
+                            predictions = detector.predict_proba(filepath)
+                            sheet[get_column_letter(cols) + str(rows)] = str(emotions[i])
+
+                            # record if the perediction is correct
+                            if(emotions[i]==max(predictions, key=predictions.get).lower()):
+                                sheet[get_column_letter(cols + 1) + str(rows)] = "correct"
+                                cols += 2        
+                            else:
+                                sheet[get_column_letter(cols + 1) + str(rows)] = f"incorrect {max(predictions, key=predictions.get).lower()}"
+                                cols += 2
+
+                            # record the result
+                            for value in (predictions).values():
+                                sheet[get_column_letter(cols) + str(rows)] = value
+                                cols += 1
+
+                            rows += 1
+                            cols = 1
+                    
+                    sheet[get_column_letter(cols) + str(rows)] = "Nene audio"
+                    rows += 1
+
+                    # Predict Nene audio
+                    for audio in os.listdir(f'predict_from_audio/Nene_{model_ver[:3]}'):
+                        sentiment, _ = audio.split("_")
+
+                        # Get prediction and record the correct sentiment
+                        predictions = detector.predict_proba(f'predict_from_audio/Nene_{model_ver[:3]}/{audio}')
+                        sheet[get_column_letter(cols) + str(rows)] = str(sentiment)
+
+                        # record if the perediction is correct
+                        if(sentiment==max(predictions, key=predictions.get).lower()):
+                            sheet[get_column_letter(cols + 1) + str(rows)] = "correct"
+                            cols += 2        
+                        else:
+                            sheet[get_column_letter(cols + 1) + str(rows)] = f"incorrect {max(predictions, key=predictions.get).lower()}"
+                            cols += 2
+  
+                        # record the result
+                        for value in (predictions).values():
+                            sheet[get_column_letter(cols) + str(rows)] = value
+                            cols += 1
+
+                        rows += 1
+                        cols = 1
+                    
+                    # predict JL corpus audio
+                    sheet[get_column_letter(cols) + str(rows)] = "JLCorpus audio"
+                    rows += 1
+                    
+                    for audio in os.listdir(f'predict_from_audio/JL_{model_ver[:3]}'):
+                        gender, sentiment, _1, _2 = audio.split("_")
+                        # predict
+                        predictions = detector.predict_proba(f'predict_from_audio/JL_{model_ver[:3]}/{audio}')
+                        sheet[get_column_letter(cols) + str(rows)] = str(sentiment)
+
+                        # check if prediction is correcyt
+                        if(sentiment==max(predictions, key=predictions.get).lower()):
+                            sheet[get_column_letter(cols + 1) + str(rows)] = "correct"
+                            cols += 2        
+                        else:
+                            sheet[get_column_letter(cols + 1) + str(rows)] = f"incorrect {max(predictions, key=predictions.get).lower()}"
+                            cols += 2
+
+                        # record prediction
+                        for value in (predictions).values():
+                            sheet[get_column_letter(cols) + str(rows)] = value
+                            cols += 1
+
+                        rows += 1
+                        cols = 1
+
+                    wb.save('predict_from_audio/prediction.xlsx')
+                    print('predictions saved to predict_from_audio/prediction.xlsx')
         
+                # else if outputting to text
+                else:
+                    # open file to write predictions on 
+                    with open(file = 'predict_from_audio' + os.sep + 'predictions.txt', mode  = 'w') as file:
+
+                        # iterate through all the files
+                        file.write("results," + str(emotions) + "\n")
+                        for emotion in emotions:
+
+                            # record emotions to predict to write to the putput file later
+                            to_predict = emotion + (8-len(emotion))*(" ")
+
+                            for filepath in glob.iglob(os.path.join("predict_from_audio",f"emotion testing audio {model_ver[:3]}", f"{emotion}/*")):
+                                
+                                # write if prediction was correct
+                                if(emotion == detector.predict(filepath).lower()):
+                                    file.write(to_predict + " correct   :" )
+                                else:
+                                    file.write(to_predict + " incorrect :" )
+                                
+                                # Write probabiltiy distribution
+                                for value in (detector.predict_proba(filepath)).values():
+                                    file.write(str(value) + ",")
+                                file.write(str(filepath) + "\n")
+                            
+                    print('predictions saved to predict_from_audio/prediction.txt')
         else:
             print('Training not implemented here yet')
     
