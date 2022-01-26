50ce3eb1f509bbcd33f562ab9db91f7074d1d7c0
Nene Matsuki <nenematsuki@gmail.com>
2022-01-26 20:57:38 +0000

    grid search on MLP classifier

--- grid_search.py
+++ grid_search.py
@@ -36,19 +36,19 @@ pickle.dump(best_estimators, open(f"grid/best_classifiers.pickle", "wb"))
 
 best_estimators = []
 
-for model, params in regression_grid_parameters.items():
-    if model.__class__.__name__ == "KNeighborsRegressor":
-        # in case of a K-Nearest neighbors algorithm
-        # set number of neighbors to the length of emotions
-        params['n_neighbors'] = [len(emotions)]
-    d = EmotionRecognizer(model, emotions=emotions, classification=False)
-    d.load_data()
-    best_estimator, best_params, cv_best_score = d.grid_search(params=params, n_jobs=n_jobs)
-    best_estimators.append((best_estimator, best_params, cv_best_score))
-    print(f"{emotions} {best_estimator.__class__.__name__} achieved {cv_best_score:.3f} cross validation MAE score!")
+# for model, params in regression_grid_parameters.items():
+#     if model.__class__.__name__ == "KNeighborsRegressor":
+#         # in case of a K-Nearest neighbors algorithm
+#         # set number of neighbors to the length of emotions
+#         params['n_neighbors'] = [len(emotions)]
+#     d = EmotionRecognizer(model, emotions=emotions, classification=False)
+#     d.load_data()
+#     best_estimator, best_params, cv_best_score = d.grid_search(params=params, n_jobs=n_jobs)
+#     best_estimators.append((best_estimator, best_params, cv_best_score))
+#     print(f"{emotions} {best_estimator.__class__.__name__} achieved {cv_best_score:.3f} cross validation MAE score!")
 
-print(f"[+] Pickling best regressors for {emotions}...")
-pickle.dump(best_estimators, open(f"grid/best_regressors.pickle", "wb"))
+# print(f"[+] Pickling best regressors for {emotions}...")
+# pickle.dump(best_estimators, open(f"grid/best_regressors.pickle", "wb"))
 
 
 
